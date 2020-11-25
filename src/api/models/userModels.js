aad8681434517a12338981e2a9eb8378eb2b4f6a
DEADL0CK <Hydrark07@gmail.com>
2020-11-25 12:48:32 +0100

    User models + routes / School Model

---
+++ src/api/models/userModels.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    email: {
+        type: String,
+        required: "L'email est requis",
+        unique: true
+    },
+    password: {
+        type: String,
+        required: "Le mdp est requis"
+    },
+    name: {
+        type: String,
+        required: "Merci d'indiqué le nom de la personne"
+    },
+    school_id:{
+        type:String
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ src/api/routes/userRoute.js
@@ -0,0 +1,8 @@
+module.exports = (server) => {
+    const userController= require('../controllers/userController');
+
+    server.route('/users/register').post(userController.create_an_user)
+    server.route('/users/login').post(userController.login_an_user)
+    
+
+}
