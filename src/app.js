cf38dcaab8c2c79d443077b37e8c7f5c9eae419d
pointk <pointk@geeps.centralesupelec.fr>
2020-11-25 10:45:44 +0100

    Set default configuration

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const server = express();
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/apinodejs')
+// moongoose.connect('mongodb://localhost:27017/apinodejs');  // Without docker
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+// const postRoute = require('./api/routes/postRoute');
+// const commentRoute = require('./api/routes/commentRoute');
+// const userRoute = require('./api/routes/userRoute');
+
+// postRoute(server);
+// commentRoute(server);
+// userRoute(server);
+
+server.listen(port, hostname);
