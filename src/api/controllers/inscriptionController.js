05cff37039c84c3856137073c35f571beca59694
MKaziel <mkaziel@hotmail.com>
2020-11-26 12:24:47 +0100

    Update inscriptionController.js

--- src/api/controllers/inscriptionController.js
+++ src/api/controllers/inscriptionController.js
@@ -1,24 +1,81 @@
 const middleware = require('../middlewares/jwtMiddleware');
+const Inscription = require('../models/inscriptionModel');
 
 exports.list_all_inscription = (request,response) => {
-    return null;
-}
+    Inscription.find({}, (error, inscriptions) => {
+        if (error) {
+            response.status(500);
+            console.log(error);
+            response.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            response.status(200);
+            response.json(inscriptions)
+        }
+    })}
 
 exports.register_inscription = (request,response) => {
-    return null;
+    let new_inscription = new Inscription(request.body);
+    new_inscription.save((error, inscription) => {
+        if (error) {
+            response.status(500);
+            console.log(error);
+            response.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            response.status(201);
+            response.json(inscription)
+        }
+    })
 }
 
 exports.get_an_inscription = (request,response) => {
-    return null;
-}
+    Inscription.findById(request.params.inscription_id, (error, inscription) => {
+        if (error) {
+            response.status(500);
+            console.log(error);
+            response.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            response.status(200);
+            response.json(inscription)
+        }
+    })}
 
 exports.update_an_inscription = (request,response) => {
-    return null;
-}
+    Inscription.findByIdAndUpdate(request.params.inscription_id, request.body, {
+        new: true
+    }, (error, inscription) => {
+        if (error) {
+            response.status(500);
+            console.log(error);
+            response.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            response.status(200);
+            response.json(inscription)
+        }
+    })}
 
 exports.delete_an_inscription = (request,response) => {
-    return null;
-}
+    Inscription.findByIdAndRemove(request.params.inscription_id, (error, inscription) => {
+        if (error) {
+            response.status(500);
+            console.log(error);
+            response.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            response.status(200);
+            response.json({
+                message: "Inscription supprimé !"
+            })
+        }
+    })}
 
 exports.get_all_inscriptions_of_team = (request,response) => {
     return null;
