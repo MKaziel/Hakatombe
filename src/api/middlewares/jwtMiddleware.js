49717ae9d7c7a0cf39dacc6747d1bee0e779accc
MKaziel <mkaziel@hotmail.com>
2020-11-26 18:25:58 +0100

    Update middleware
    Add a function to verify the role of the user.

--- src/api/middlewares/jwtMiddleware.js
+++ src/api/middlewares/jwtMiddleware.js
@@ -16,6 +16,31 @@ exports.verify_token = (req,res,next) => {
         })
     } else {
         res.status(403)
-        res.json({message:"Accès interdit mon pote !!!!!!"})
+        res.json({message:"Pour avancer, te connecter tu dois."})
+    }
+}
+
+exports.verify_token_and_roles = (req,res,next) => {
+    let token = req.headers['authorization'];
+    let role = req.headers['role'];
+    if(typeof token != 'undefined'){
+        jwt.verify(token, JWT_SECRET, (error) => {
+            if(error){
+                res.sendStatus(403)
+            }
+            else {
+                userRole = jwt.decode(token);
+                if(role == userRole.role){
+                    next();
+                    console.log(jwt.decode(token)); 
+                } else {
+                    res.status(403)
+                    res.json({message:"PLus loin tu n'iras pas !"});
+                }
+            }
+        })
+    } else {
+        res.status(403)
+        res.json({message:"Pour avancer, te connecter tu dois."})
     }
 }
