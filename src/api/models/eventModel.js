0ae111ff1d48cc1a2955d0e9a553b0dfc252a3f6
MKaziel <mkaziel@hotmail.com>
2020-11-26 12:10:29 +0100

    Update eventModel.js

--- src/api/models/eventModel.js
+++ src/api/models/eventModel.js
@@ -14,6 +14,6 @@ let eventSchema = new Schema({
         type:String,
         required: "Veuillez décrire l'évènement"
     }
-})
+});
 
-module.exports = mongoose.model('Event', eventSchema)
+module.exports = mongoose.model('Event', eventSchema);
