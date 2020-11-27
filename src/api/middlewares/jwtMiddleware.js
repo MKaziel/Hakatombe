66387367030313dfade32264e362a1f0b5a20671
MKaziel <mkaziel@hotmail.com>
2020-11-27 09:25:12 +0100

    Update jwtMiddleware.js

--- src/api/middlewares/jwtMiddleware.js
+++ src/api/middlewares/jwtMiddleware.js
@@ -20,9 +20,110 @@ exports.verify_token = (req,res,next) => {
     }
 }
 
+//Exemple de fonction pour gérer un role fourni dans le header de la requête
 exports.verify_token_and_roles = (req,res,next) => {
     let token = req.headers['authorization'];
     let role = req.headers['role'];
+    if(typeof token != 'undefined'){
+        jwt.verify(token, JWT_SECRET, (error) => {
+            if(error){
+                res.sendStatus(403)
+            }
+            else {
+                userRole = jwt.decode(token);
+                if(role == userRole.role){
+                    next();
+                    console.log(jwt.decode(token)); 
+                } else {
+                    res.status(403)
+                    res.json({message:"PLus loin tu n'iras pas !"});
+                }
+            }
+        })
+    } else {
+        res.status(403)
+        res.json({message:"Pour avancer, te connecter tu dois."})
+    }
+}
+
+exports.verify_token_admin = (req,res,next) => {
+    let token = req.headers['authorization'];
+    let role = "admin";
+    if(typeof token != 'undefined'){
+        jwt.verify(token, JWT_SECRET, (error) => {
+            if(error){
+                res.sendStatus(403)
+            }
+            else {
+                userRole = jwt.decode(token);
+                if(role == userRole.role){
+                    next();
+                    console.log(jwt.decode(token)); 
+                } else {
+                    res.status(403)
+                    res.json({message:"PLus loin tu n'iras pas !"});
+                }
+            }
+        })
+    } else {
+        res.status(403)
+        res.json({message:"Pour avancer, te connecter tu dois."})
+    }
+}
+
+exports.verify_token_tl = (req,res,next) => {
+    let token = req.headers['authorization'];
+    let role = "tl";
+    if(typeof token != 'undefined'){
+        jwt.verify(token, JWT_SECRET, (error) => {
+            if(error){
+                res.sendStatus(403)
+            }
+            else {
+                userRole = jwt.decode(token);
+                if(role == userRole.role){
+                    next();
+                    console.log(jwt.decode(token)); 
+                } else {
+                    res.status(403)
+                    res.json({message:"PLus loin tu n'iras pas !"});
+                }
+            }
+        })
+    } else {
+        res.status(403)
+        res.json({message:"Pour avancer, te connecter tu dois."})
+    }
+}
+
+exports.verify_token_tm = (req,res,next) => {
+    let token = req.headers['authorization'];
+    let role = "tm";
+    if(typeof token != 'undefined'){
+        jwt.verify(token, JWT_SECRET, (error) => {
+            if(error){
+                res.sendStatus(403)
+            }
+            else {
+                userRole = jwt.decode(token);
+                if(role == userRole.role){
+                    next();
+                    console.log(jwt.decode(token)); 
+                } else {
+                    res.status(403)
+                    res.json({message:"PLus loin tu n'iras pas !"});
+                }
+            }
+        })
+    } else {
+        res.status(403)
+        res.json({message:"Pour avancer, te connecter tu dois."})
+    }
+}
+
+exports.verify_token_user = (req,res,next) => {
+    let token = req.headers['authorization'];
+    let role = "user";
     if(typeof token != 'undefined'){
         jwt.verify(token, JWT_SECRET, (error) => {
             if(error){
